75c23499afe9f92c7bcaf78a504be990567efdd9
NureMaksymenkoMarko <marko.maksymenko@nure.ua>
2026-05-07 19:41:13 +0300

    Update treatmentRouter.js

--- src/routes/treatmentRouter.js
+++ src/routes/treatmentRouter.js
@@ -7,9 +7,11 @@ const authMiddleware = require("../middleware/auth.js");
 const {
   getTreatmentById,
   getAllTreatments,
+  getTreatmentsByUser,
 } = require("../controllers/treatmentController");
 
 userRouter.get("/treatment", adminMiddleware, getAllTreatments);
 userRouter.get("/treatment/:id", authMiddleware, getTreatmentById);
+userRouter.get("/users/:userId/treatments", authMiddleware, getTreatmentsByUser);
 
 module.exports = userRouter;
