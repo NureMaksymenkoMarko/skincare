675f01ebc6c23e7a7295288eee68748eaa326b12
NureMaksymenkoMarko <marko.maksymenko@nure.ua>
2026-05-07 19:44:37 +0300

    Update analysisRouter.js

--- src/routes/analysisRouter.js
+++ src/routes/analysisRouter.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const adminMiddleware = require("../middleware/isAdmin.js");
 const authMiddleware = require("../middleware/auth.js");
 
@@ -8,9 +7,11 @@ const userRouter = express.Router();
 const {
   getAnalysisById,
   getAllAnalyses,
+  getAnalysesBySkin,
 } = require("../controllers/analysisController");
 
 userRouter.get("/analysis", adminMiddleware, getAllAnalyses);
 userRouter.get("/analysis/:id", authMiddleware, getAnalysisById);
+userRouter.get("/skin/:skinId/analyses", authMiddleware, getAnalysesBySkin);
 
 module.exports = userRouter;
