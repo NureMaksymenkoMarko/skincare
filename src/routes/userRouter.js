af4faf84119542e7b848b15abb29caf51418e01c
NureMaksymenkoMarko <marko.maksymenko@nure.ua>
2026-05-07 18:46:49 +0300

    Update userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -10,11 +10,13 @@ const {
   deleteUser,
   login,
   getUserByToken,
+  getAllUsers,
 } = require("../controllers/userController");
 
 userRouter.post("/login", login);
 userRouter.post("/register", createUser);
 userRouter.get("/users/me", authMiddleware, getUserByToken);
+userRouter.get("/users", authMiddleware, getAllUsers);
 userRouter.get("/users/:id", authMiddleware, getUserById);
 userRouter.put("/users/:id", authMiddleware, updateUser);
 userRouter.delete("/users/:id", authMiddleware, deleteUser);
