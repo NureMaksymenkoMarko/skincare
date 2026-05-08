3bb5c2733af19cbc0293e709d397de6496b0ac20
NureMaksymenkoMarko <marko.maksymenko@nure.ua>
2026-05-08 12:56:10 +0300

    Update skinRouter.js

--- src/routes/skinRouter.js
+++ src/routes/skinRouter.js
@@ -4,9 +4,14 @@ const userRouter = express.Router();
 const adminMiddleware = require("../middleware/isAdmin.js");
 const authMiddleware = require("../middleware/auth.js");
 
-const { getSkinById, getAllSkins } = require("../controllers/skinController");
+const { 
+  getSkinById, 
+  getAllSkins, 
+  getSkinByUserId 
+} = require("../controllers/skinController");
 
 userRouter.get("/skin", adminMiddleware, getAllSkins);
 userRouter.get("/skin/:id", authMiddleware, getSkinById);
+userRouter.get("/users/:userId/skin", authMiddleware, getSkinByUserId);
 
 module.exports = userRouter;
